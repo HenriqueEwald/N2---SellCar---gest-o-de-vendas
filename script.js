a5fa448111a8b97552a2f9a203d505e78217a6ec
Felipe-Mattos07 <felipe.mattos@catolicasc.edu.br>
2026-05-31 22:08:19 -0300

    Integração do JS com HTML

--- script.js
+++ script.js
@@ -4,13 +4,38 @@ let usuarios = [];
 async function buscarPosts() {
   const resposta = await fetch("https://jsonplaceholder.typicode.com/posts");
   posts = await resposta.json();
-  console.log("Posts carregados:", posts);
 }
 
 async function buscarUsuarios() {
   const resposta = await fetch("https://jsonplaceholder.typicode.com/users");
   usuarios = await resposta.json();
-  console.log("Usuários carregados:", usuarios);
+  exibirUsuarios();
+}
+
+function exibirUsuarios() {
+  const lista = document.getElementById("list-user");
+  const selectCadastro = document.getElementById("f-vendedor");
+  const selectEdicao = document.getElementById("edit-vendedor");
+
+  lista.innerHTML = "";
+  selectCadastro.innerHTML = "";
+  selectEdicao.innerHTML = "";
+
+  usuarios.forEach(function (usuario) {
+    const item = document.createElement("li");
+    item.textContent = usuario.name;
+    lista.appendChild(item);
+
+    const opcao1 = document.createElement("option");
+    opcao1.value = usuario.id;
+    opcao1.textContent = usuario.name;
+    selectCadastro.appendChild(opcao1);
+
+    const opcao2 = document.createElement("option");
+    opcao2.value = usuario.id;
+    opcao2.textContent = usuario.name;
+    selectEdicao.appendChild(opcao2);
+  });
 }
 
 buscarPosts();
